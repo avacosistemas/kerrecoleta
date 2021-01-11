bfec667fabc40767456baddd101238ab2074b495
gonzalooluna <gonzaloo.luna@hotmail.com>
2021-01-11 17:50:03 -0300

    prueba de agregar boton de whatsapp desde un include

--- contacto.php
+++ contacto.php
@@ -129,7 +129,7 @@ Estación de micros (Retiro): 10 minutos<br>
 	        
 	        <div class="clear"></div>
         </div>
-        
+        <?php include("mensaje_whatsapp.php");?>
         <?php include("footer.php");?>
 <script src="js/jquery.bxslider1/jquery.bxslider.js"></script>
 		<link rel="stylesheet" href="js/jquery.bxslider1/jquery.bxslider.css">
